822947e5f74efedaa6bf98fba408f36837714029
Jason Liao <liao.jason2@gmail.com>
2024-12-10 14:11:32 +0800

    把 azure function 模型換成 v2

--- MyProjFolder/function_app.py
+++ MyProjFolder/function_app.py
@@ -2,7 +2,10 @@ import logging
 import azure.functions as func
 from bs4 import BeautifulSoup
 
-def main(req: func.HttpRequest) -> func.HttpResponse:
+app = func.FunctionApp()
+
+@app.route(route="MyHttpTrigger", auth_level=func.AuthLevel.FUNCTION)
+def MyHttpTrigger(req: func.HttpRequest) -> func.HttpResponse:
     logging.info('Python HTTP trigger function processed a request.')
 
     name = req.params.get('name')
